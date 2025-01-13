ac89286450f909a0ba7cd7f50944f2d9951e09de
Sanaan7788 <sanaan7788@gmail.com>
2025-01-13 11:39:02 -0600

    created streamlit app

---
+++ home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("Welcome to my first LLM request")
+st.header("OpenAI API")
+openai_prompt=st.text_input("Please type your prompt")
+
+if st.button("Send"):
+    #gpt methods
+    st.success("Content Generated!")
+else:
+    st.warning("Please insert a prompt")
+
+st.divider()
+
+st.header("Gemini API")
+gemini_prompt=st.text_input("Please type your prompt")
+gemini_tokens=st.number_input("Please select number of tokens")
+if st.button("Send"):
+    #gpt methods
+    st.success("Content Generated!")
+else:
+    st.warning("Please insert a prompt")
