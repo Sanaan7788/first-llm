d6c8964e6a08e2cb250119083fb11dec767c9976
Sanaan7788 <sanaan7788@gmail.com>
2025-01-12 22:53:41 -0600

    added method gemini for text calling

--- gemini-wrapper.py
+++ gemini-wrapper.py
@@ -4,15 +4,15 @@ import google.generativeai as genai
 import streamlit as st
 
 api_key=st.secrets["api_key_google"]
-genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content(
-    "Explain how AI works",
-    generation_config = genai.GenerationConfig(
-        max_output_tokens=100,
-        temperature=0.1,
+def generate_gemini_text(prompt,tokens):
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(
+        prompt,
+        generation_config = genai.GenerationConfig(
+            max_output_tokens=tokens,
+            temperature=0.1,
+        )
     )
-)
-
-print(response.text)
+    return response.text
