b5d04c67d0aa2b388eb543f8419a5caede2e0a37
Sanaan7788 <sanaan7788@gmail.com>
2025-01-12 22:50:02 -0600

    created gemini wrapper-text generation

--- gemini-wrapper.py
+++ gemini-wrapper.py
@@ -1,1 +1,18 @@
 #this file contains gemini connection
+
+import google.generativeai as genai
+import streamlit as st
+
+api_key=st.secrets["api_key_google"]
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content(
+    "Explain how AI works",
+    generation_config = genai.GenerationConfig(
+        max_output_tokens=100,
+        temperature=0.1,
+    )
+)
+
+print(response.text)
