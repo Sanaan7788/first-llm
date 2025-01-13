f0f2bd7965ff352e10246013e7dd6650d7f309bd
Sanaan7788 <sanaan7788@gmail.com>
2025-01-12 18:34:07 -0600

    added some openai code in gpt-wrapper and also added streamlit

--- gpt-wrapper.py
+++ gpt-wrapper.py
@@ -1,1 +1,14 @@
 #this file contains gpt connection
+
+from openai import OpenAI
+import streamlit as st
+
+api_key = st.secrets["api_key"]
+client = OpenAI(api_key=api_key)
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": "write a haiku about ai"}
+    ]
+)
+print(completion.choices[0].message.content)
